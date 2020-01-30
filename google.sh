fc1ceb7d2db34b1d9e875b8dbb7f8d452d0cf7f3
semblack <49827894+semblack@users.noreply.github.com>
2020-01-30 22:37:29 +0800

    Update google.sh

--- google.sh
+++ google.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # apt-get install ninja-build libprotobuf-dev protobuf-compiler -y
 
-rm -f kaggle
-rm -f lc0
-
 wget -c https://github.com/semblack/kaggle/raw/master/kaggle
 wget -c https://github.com/semblack/kaggle/raw/master/lc0
 chmod 777 kaggle
